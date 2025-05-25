3bc2f9f813bac58867586927ea180c045974e496
lucianoescuderopalma <airaoinvasor055@gmail.com>
2025-05-24 22:04:06 -0400

    Update CursoService.java

--- Proyecto 2/course.manager/src/main/java/com/edutech/course/manager/service/CursoService.java
+++ Proyecto 2/course.manager/src/main/java/com/edutech/course/manager/service/CursoService.java
@@ -3,6 +3,7 @@ package com.edutech.course.manager.service;
 import com.edutech.course.manager.model.Curso;
 import com.edutech.course.manager.repository.CursoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,10 +15,19 @@ public class CursoService {
     private CursoRepository repo;
 
     public Curso crear(Curso curso) { return repo.save(curso); }
+
+    public Optional<Curso> obtenerCurso(Long id) {
+        return repo.findById(id);
+    }
+
     public List<Curso> listar() { return repo.findAll(); }
+
     public Optional<Curso> obtener(Long id) { return repo.findById(id); }
+
     public void eliminar(Long id) { repo.deleteById(id); }
+
     public Curso actualizar(Curso c) { return repo.save(c); }
+
     public Curso aprobarContenido(Long id) {
         Curso c = repo.findById(id).orElseThrow();
         c.setAprobado(true);
