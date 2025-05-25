41e016704a4b9d95ba9a217895487679db5ad111
lucianoescuderopalma <airaoinvasor055@gmail.com>
2025-05-24 22:01:38 -0400

    Update CursoController.java

--- Proyecto 2/course.manager/src/main/java/com/edutech/course/manager/controller/CursoController.java
+++ Proyecto 2/course.manager/src/main/java/com/edutech/course/manager/controller/CursoController.java
@@ -1,13 +1,15 @@
 package com.edutech.course.manager.controller;
 
 import com.edutech.course.manager.model.Curso;
-import com.edutech.course.manager.service.CursoService; // Asegúrate de que este import exista
+import com.edutech.course.manager.service.CursoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/cursos")
@@ -42,4 +44,11 @@ public class CursoController {
     public ResponseEntity<Curso> aprobar(@PathVariable Long id) {
         return ResponseEntity.ok(service.aprobarContenido(id));
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<Curso> obtenerPorId(@PathVariable Long id) {
+        Optional<Curso> curso = service.obtenerCurso(id);
+        return curso.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
 }
